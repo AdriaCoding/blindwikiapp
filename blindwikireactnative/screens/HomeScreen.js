b4bbae334b3d314b8f70410fd23692210aa6b162
AdriaCoding <adrialisa99@gmail.com>
2025-02-17 13:32:50 +0100

    Recording button is a pressable element without special component.

--- blindwikireactnative/screens/HomeScreen.js
+++ blindwikireactnative/screens/HomeScreen.js
@@ -1,15 +1,19 @@
-import { View, Text, StyleSheet, Button} from "react-native";
+import { View, Text, StyleSheet, Button, Pressable} from "react-native";
 import InstructionsText from "../components/InstructionsText";
 import Location from "../components/Location";
+
+function record() {
+  console.log('Recording...');
+}
 function HomeScreen() {
   return (
     <View>
       <InstructionsText text="Prem el següent botó per actualitzar la teva ubicació GPS"/>
       <Location text="Actualitzar ubicació"/>
       <InstructionsText text="Prem el següent botó 'Gravar' per iniciar la gravació de so. Prem-lo novament per acabar la gravació"/>
-      <View>
-        <Button title="Gravar"/>
-      </View>
+      <Pressable style={styles.buttonContainer} onPress={record}>
+        <Text style={styles.buttonText}>Gravar</Text>  
+      </Pressable>
     </View>
   );
 }
@@ -17,5 +21,20 @@ function HomeScreen() {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-
+  buttonContainer: {
+    margin: 15,
+    alignSelf: "center",
+    width: "95%",
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+    borderWidth: 2,
+    margin: 10,
+    padding: 25,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+  }
 });
