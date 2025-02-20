4a39da6ed84b843ba062a6a062fb343e2366565c
AdriaCoding <adrialisa99@gmail.com>
2025-02-20 13:00:19 +0100

    Moved my homepage into tsx code

--- TSRN/app/(tabs)/index.tsx
+++ TSRN/app/(tabs)/index.tsx
@@ -1,31 +1,43 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Pressable} from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
+import InstructionsText from '@/components/InstructionsText';
+import Location from '@/components/Location';
 import { Text, View } from '@/components/Themed';
 
-export default function TabOneScreen() {
+function record() {
+  console.log('Recording...');
+}
+const ubicació = () => {
+  return "Carrer de Sant Antoni Maria Claret, 171, Horta-Guinardó, 08041 Barcelona, España : Alta Precisió del GPS";
+}
+
+export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View>
+      <InstructionsText text="Prem el següent botó per actualitzar la teva ubicació GPS"/>
+      <Location location={ubicació()} />
+      <InstructionsText text="Prem el següent botó 'Gravar' per iniciar la gravació de so. Prem-lo novament per acabar la gravació"/>
+      <Pressable style={styles.buttonContainer} onPress={record}>
+        <Text style={styles.buttonText}>Gravar</Text>  
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  buttonContainer: {
+    margin: 15,
+    alignSelf: "center",
+    width: "95%",
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+    borderWidth: 2,
+    padding: 25,
   },
-  title: {
+  buttonText: {
+    color: "white",
     fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  }
 });
