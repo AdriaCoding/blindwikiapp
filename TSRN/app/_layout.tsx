f916e38a6c46d0e4948d91ae655e26fdc60c80b0
AdriaCoding <adrialisa99@gmail.com>
2025-02-20 13:51:57 +0100

    Change router names

--- TSRN/app/_layout.tsx
+++ TSRN/app/_layout.tsx
@@ -1,5 +1,4 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -13,7 +12,7 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: 'screens',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -46,11 +45,9 @@ export default function RootLayout() {
 function RootLayoutNav() {
   //<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
   return (
-    <ThemeProvider value={DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
-    </ThemeProvider>
   );
 }
