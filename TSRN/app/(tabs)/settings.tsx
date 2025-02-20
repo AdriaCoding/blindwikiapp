d925b963dc86b371455f9c4dde2db1b436f3bf98
AdriaCoding <adrialisa99@gmail.com>
2025-02-20 13:47:06 +0100

    Change second screen + remove Edit screen info

--- TSRN/app/(tabs)/settings.tsx
+++ TSRN/app/(tabs)/settings.tsx
@@ -1,14 +1,10 @@
-import { StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from 'react-native';
-
-export default function TabTwoScreen() {
+export default function Settings() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Settings</Text>
       <View style={styles.separator}/>
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
 }
