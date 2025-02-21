50c67e43e24ada51a075ab4171772b10b61f1273
AdriaCoding <adrialisa99@gmail.com>
2025-02-21 13:18:54 +0100

    Adding packages and i18n class

---
+++ TSRN/locales/i18n.ts
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import en from './en.json';
+import es from './es.json';
+import ca from './ca.json';
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: en },
+    es: { translation: es },
+    ca: { translation: ca },
+  },
+  lng: 'ca',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
